853ebaa86d4fb14412d9afaa4a201cc32bca0476
Aichurek Zhalilova <anadan20182019@gmail.com>
2019-07-20 13:46:05 +0000

    Heroku2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-     include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
   #storage :file
   storage :fog
@@ -12,7 +11,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  process resize_to_fill: [800, 350]
+  #process resize_to_fill: [800, 350]
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
