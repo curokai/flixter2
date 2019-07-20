9defcd6ebce5376bf3d00bb92e188dfc82a4aaf1
Aichurek Zhalilova <anadan20182019@gmail.com>
2019-07-20 15:01:32 +0000

    H3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-devise_for :users do
-  get '/users/sign_out' => 'devise/sessions#destroy'
-end
+devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   resources :lessons, only: [:show]
